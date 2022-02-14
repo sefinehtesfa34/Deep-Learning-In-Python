3841e9a7cb51fa2e39e861bf4cfba4aad7371eb3
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-02-14 02:19:06 -0800

    FashionMNIST datasets model prediction using sequential model in pytorch

--- pytorch/fashioMNIST.py
+++ pytorch/fashioMNIST.py
@@ -1,3 +1,6 @@
+
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# print(f'Using {device} device')
 import torch
 from torch.utils.data import DataLoader
 import torchvision
@@ -79,7 +82,14 @@ print(f"Label: {label}")
 
 #Transforms to Tensor and to One-Hot Encoding of 
 # the features and the labels respectively
+# ToTensor()
+# ToTensor converts a PIL image or NumPy ndarray into a FloatTensor. and scales the image’s pixel intensity values in the range [0., 1.]
 
+# Lambda Transforms
+# Lambda transforms apply any user-defined lambda function. Here, we define a function to turn the integer into a one-hot encoded tensor. It first creates a zero tensor of size 10 (the number of labels in our dataset) and calls scatter_ which assigns a value=1 on the index as given by the label y.
+
+# target_transform = Lambda(lambda y: torch.zeros(
+#     10, dtype=
 from torchvision.transforms import ToTensor, Lambda
 
 ds = datasets.FashionMNIST(
@@ -87,18 +97,70 @@ ds = datasets.FashionMNIST(
     train=True,
     download=True,
     transform=ToTensor(),
-    target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
+    target_transform=Lambda(
+        lambda y: torch.zeros(10, 
+                    dtype=torch.float).scatter_(0, 
+                    torch.tensor(y), value=1),
+                    torch.float)
+                    .scatter_(dim=0, 
+                              index=torch.tensor(y), 
+                              value=1))
+
+
+#Defining the class or the pytorch model
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+model = NeuralNetwork().to(device)
+print(model)
+#Passing the input data
+X = torch.rand(1, 28, 28, device=device)
+logits = model(X)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
+
+#Let us see this model on images
+input_image = torch.rand(3,28,28)
+print(input_image.size())
+flatten = nn.Flatten()
+flat_image = flatten(input_image)
+print(flat_image.size())
+
+layer1 = nn.Linear(in_features=28*28, out_features=20)
+hidden1 = layer1(flat_image)
+print(hidden1.size())
+
+print(f"Before ReLU: {hidden1}\n\n")
+hidden1 = nn.ReLU()(hidden1)
+print(f"After ReLU: {hidden1}")
+seq_modules = nn.Sequential(
+    flatten,
+    layer1,
+    nn.ReLU(),
+    nn.Linear(20, 10)
 )
-
-
-
-
-
-
-
-
-
-
+input_image = torch.rand(3,28,28)
+logits = seq_modules(input_image)
+softmax = nn.Softmax(dim=1)
+pred_probab = softmax(logits)
 
 
 
