5144f63329f35150921bc6f326c0a03b10cfa38f
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-02-14 21:59:20 -0800

    MLP mnist

--- pytorch/trainAmodel.py
+++ pytorch/trainAmodel.py
@@ -16,21 +16,22 @@ train_data=datasets.FashionMNIST(
     )
 trainloader = DataLoader(train_data, batch_size=64, shuffle=True)
 class Classifier(nn.Module):
-      def __init__(self):
-    super().__init__()
+    def __init__(self):
+        super().__init__()
 
-    self.layer1=nn.Linear(784,256)
-    self.layer2=nn.Linear(256,128)
-    self.layer3=nn.Linear(128,64)
-    self.outputLayer=nn.Linear(64,10)
-  def forward(self,x):
+        self.layer1=nn.Linear(784,256)
+        self.layer2=nn.Linear(256,128)
+        self.layer3=nn.Linear(128,64)
+        self.outputLayer=nn.Linear(64,10)
+        self.dropout=nn.Drpout(p=0.2)
+    def forward(self,x):
     # Flatten the image
-    x=x.view(x.shape[0],-1)
-    x=F.relu(self.layer1(x))
-    x=F.relu(self.layer2(x))
-    x=F.relu(self.layer3(x))
-    x=F.log_softmax(self.outputLayer(x),dim=1)
-    return x
+        x=x.view(x.shape[0],-1)
+        x=self.dropout(F.relu(self.layer1(x)))
+        x=self.dropout(F.relu(self.layer2(x)))
+        x=self.dropout(F.relu(self.layer3(x)))
+        x=F.log_softmax(self.outputLayer(x),dim=1)
+        return x
 model=Classifier()
 criterion=nn.NLLLoss()
 optimizer=optim.Adam(model.parameters(),lr=0.001)
